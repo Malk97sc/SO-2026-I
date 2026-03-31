07e56da41472c2d59ff96bde66eba37d47345f88
Malak <malaksanchez09@gmail.com>
2026-03-31 18:12:58 -0500

    Sum with Process Ended

--- capitulo_1/ejercicios_propuestos/codes/sum.c
+++ capitulo_1/ejercicios_propuestos/codes/sum.c
@@ -1,97 +1,130 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <wait.h>
 
 void showtree();
+void read_file(const char *file, int **vector, int *tam);
 
-void read_file(const char *file, int **vec, int *size);
-
-int main(int argc, char **argv){
+int main (int argc, char **argv){
     if(argc < 2){
-        perror("Send a file\n");
+        perror("Se necesita archivo \n");
         return EXIT_FAILURE;
     }
+    remove("out.txt");
+
     pid_t root = getpid();
-    int n_children = 8, child_id;
+    int n_child = atoi(argv[2]);
+    int child_id;
 
-    int size, chunk;
-    int *vec;
+    int tam;
+    int chunk;
+    int *vector;
 
-    read_file(argv[1], &vec, &size);
+    read_file(argv[1], &vector, &tam);
 
-    chunk = size / n_children;
-    printf("CHUNK: %d\n", chunk);
+    chunk = tam / n_child;
+    printf("Chunk: %d\n", chunk);
 
-    for(child_id = 0; child_id < n_children; child_id++){
-        if(!fork()) break;
+    for(child_id = 0; child_id < n_child; child_id++){
+        if(!fork()){
+            break;
+        }
     }
 
-    if(root == getpid()){ //proceso PADRE
+    if(root == getpid()){ //Proceso PADRE
         showtree();
-
-        for(int i=0; i < n_children; i++) wait(NULL);
-
-        //item 6...
-        
-    }else{ //procesos HIJOS
-        int idx_start, idx_end;
-
-        printf("[%d]-[CHILD_ID] = %d\n", getpid(), child_id);
-        idx_start = chunk * child_id;
-        /*if(child_id == n_children-1){
-            idx_end = size;
-        }else{
-            idx_end = chunk * (child_id+1);
-        }*/
-        idx_end = child_id == n_children-1 ? size : chunk * (child_id+1);
-        printf("[%d]-[IDX] = [%d][%d]\n\n", getpid(), idx_start, idx_end);
-
-        for(int i=idx_start; i < idx_end; i++){
-            //item 2 suma hijo
+        for(int i = 0; i < n_child; i++){
+            wait(NULL);
         }
 
-        //item 3 out.txt
+        long int sum=0, value;
+        FILE *fl = fopen("out.txt", "r");
+        if(!fl){
+            perror("Error al abrir el archivo");
+            exit(-1);
+        }
+
+        for(int i=0; i < n_child; i++){
+            fscanf(fl, "%ld", &value);
+            //printf("sum: %ld\n", value);
+            sum += value;
+        }
+
+        fclose(fl);
+
+        printf("Total SUM: %ld\n", sum);
+
+    }else{ //Proceso HIJO
+        int indx_inicio, indx_final;
+        printf("[%d] -> [CHILD_ID] = %d\n", getpid(), child_id);
+        indx_inicio = chunk * child_id;
+        if(child_id == n_child-1){
+            indx_final = tam;
+        }else{
+            indx_final = chunk * (child_id + 1);
+        }
+        //printf("[%d]-[IDX] = [%d][%d]\n\n", getpid(), indx_inicio, indx_final);
+
+        long int sum1 = 0;
+
+        FILE *f = fopen("out.txt", "a");
+        if(f == NULL){
+            perror("Error al abrir el archivo");
+            exit(-1);
+        }
+
+        for(int i = indx_inicio; i < indx_final; i++){
+            sum1 = sum1 + vector[i];
+        }
+
+        //printf("sum: %ld\n", sum1);
+
+        fprintf(f, "%ld\n", sum1);
+        fclose(f);
 
     }
 
-    free(vec);
+    free(vector);
+
     return EXIT_SUCCESS;
 }
 
-void read_file(const char *file, int **vec, int *size){
-    FILE *fl = fopen(file, "r");
-    if(!fl){
-        perror("Fail Fopen\n");
+void read_file(const char *file, int **vector, int *tam){
+    FILE *f = fopen(file, "r");
+    if(f == NULL){
+        perror("Error al abrir el archivo");
+        exit(-1);
+    }
+    
+    fscanf(f, "%d", tam);
+    printf("Size: %d\n", *tam);
+
+    *vector = (int*) malloc(*tam * sizeof(int));
+    if(!*vector){
+        perror("Fallo malooc.\n");
         exit(-1);
     }
 
-    fscanf(fl, "%d", size);
-    printf("Size: %d\n", *size);
-
-    *vec = (int*) malloc(*size * sizeof(int));
-    if(!*vec){
-        perror("Fail malloc\n");
-        exit(-1);
-    }
-
-    for(int i=0; i < *size; i++){
-        if(fscanf(fl, "%d", &(*vec)[i]) != 1){
-            fprintf(stderr, "Error: no se pudo leer el elemento %d\n", i);
+    for(int i = 0; i < *tam; i++){
+        if(fscanf(f, "%d", &(*vector)[i]) != 1){
+            fprintf(stderr, "Error no se pudo leer el elemento %d\n", i);
             exit(EXIT_FAILURE);
         }
     }
 
-    for(int i=0; i < *size; i++){
-        //printf("%d ", (*vec)[i]);
-    }
+    /*for(int i = 0; i < *tam; i++){
+        printf("%d ", (*vector)[i]);
+    }*/
 
-    fclose(fl);
+    printf("\n");
+
+    fclose(f);
 }
 
 void showtree(){
     char cmd[20] = {""};
     sprintf(cmd, "pstree -cAlp %d", getpid());
-    system(cmd);	
+    system(cmd);
 }

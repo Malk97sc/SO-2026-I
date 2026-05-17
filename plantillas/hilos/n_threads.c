7ac6287c9cdbea97f4892025d8559b7f43d28c3b
Malak <malaksanchez09@gmail.com>
2026-05-16 21:23:52 -0500

    New Threads Templates

---
+++ plantillas/hilos/n_threads.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct {
+    int id;
+    //fill
+} thread_args;
+
+void create_threads(int n_threads);
+
+void *func(void *arg);
+
+int main(int argc, char **argv){
+    if(argc < 2){
+        perror("Send n_threads");
+        return EXIT_FAILURE;
+    }
+
+    int n_threads = atoi(argv[1]);
+
+    create_threads(n_threads);
+
+    return EXIT_SUCCESS;
+}
+
+void create_threads(int n_threads){
+    pthread_t *threads; //n_threads
+    thread_args *args;
+
+    threads = (pthread_t*) malloc(n_threads * sizeof(pthread_t));
+    args = (thread_args*) malloc(n_threads * sizeof(thread_args));
+    if(!threads || !args){
+        perror("Fail malloc");
+        exit(-1);
+    }
+
+    for(int i=0; i < n_threads; i++){
+        args[i].id = i;
+        pthread_create(&threads[i], NULL, func, &args[i]);
+    }
+
+    for(int i=0; i < n_threads; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    free(threads);
+    free(args);
+}
+
+void *func(void *arg){
+    thread_args *thread = (thread_args*) arg;
+
+    printf("Thread: %d\n", thread->id);
+
+
+    return NULL;
+}
